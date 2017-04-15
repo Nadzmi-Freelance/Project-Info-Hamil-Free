05e332ba03ee58402190b5a5757d64846a0e34be
SeladangHijau <nadzmiidzham@gmail.com>
2017-04-15 18:17:33 +0600

    - put restriction to certain page based on Cik Asmawi's requirement

--- app/src/main/java/com/onepage/infohamilfree/MingguHamil.java
+++ app/src/main/java/com/onepage/infohamilfree/MingguHamil.java
@@ -1,9 +1,12 @@
 package com.onepage.infohamilfree;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.onepage.infohamilfree.adapter.ListViewMenuAdapter;
 
@@ -112,13 +116,33 @@ public class MingguHamil extends AppCompatActivity implements AdapterView.OnItem
                 }
                 break;
             case R.id.lvMingguHamil:
-                Intent nextAct;
+                if((position == 0) || (position == 9) || (position == 19) || (position == 29) || (position == 39)) {
+                    Intent nextAct;
 
-                nextAct = new Intent(this, InfoMingguHamil.class);
+                    nextAct = new Intent(this, InfoMingguHamil.class);
 
-                nextAct.putExtra("mingguPos", position);
-                nextAct.putExtra("title", mingguHamil[position]);
-                startActivity(nextAct);
+                    nextAct.putExtra("mingguPos", position);
+                    nextAct.putExtra("title", mingguHamil[position]);
+                    startActivity(nextAct);
+                } else {
+                    AlertDialog alertDialog = new AlertDialog.Builder(this)
+                            .setTitle(R.string.tajuk_alert_free_version)
+                            .setMessage(R.string.desc_alert_free_version_minggu_hamil)
+                            .setPositiveButton(R.string.nav_alert_free_version_positive, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent gotoPlayStore;
+
+                                    gotoPlayStore = new Intent(Intent.ACTION_VIEW, Uri.parse(getResources().getString(R.string.nav_goto_full_version)));
+
+                                    startActivity(gotoPlayStore);
+                                }
+                            })
+                            .setNegativeButton(R.string.nav_alert_free_version_negative, null)
+                            .create();
+
+                    alertDialog.show();
+                }
                 break;
         }
     }
